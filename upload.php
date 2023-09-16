cdae5837483809cddbc370b77f58ee08a13c0323
CODEDBOY11 <143560887+CODEDBOY11@users.noreply.github.com>
2023-09-16 21:16:14 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,20 +1,13 @@
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["upload-song"])) {
-    $targetDirectory = "uploads/"; // Create an "uploads" directory where the songs will be stored
-    $targetFile = $targetDirectory . basename($_FILES["song-file"]["name"]);
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["upload"])) {
+    $targetDirectory = "uploads/"; // Create an "uploads" directory where the files will be stored
+    $targetFile = $targetDirectory . basename($_FILES["file"]["name"]);
     
-    // Check if the file is an allowed format (e.g., mp3 or wav)
-    $allowedExtensions = array("mp3", "wav");
-    $fileExtension = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-    
-    if (in_array($fileExtension, $allowedExtensions)) {
-        // Move the uploaded file to the server
-        if (move_uploaded_file($_FILES["song-file"]["tmp_name"], $targetFile)) {
-            // Redirect to the index.html page with the uploaded_file parameter
-            header("Location: index.html?uploaded_file=" . $targetFile);
-            exit(); // Terminate script execution after the redirect
-        } else {
-            echo "Error uploading the file.";
-        }
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile)) {
+        header("Location: index.html"); // Redirect back to the index page after successful upload
+        exit();
     } else {
-        echo "Invalid file format. Please upload an mp3 or wav file.";
+        echo "Error uploading the file.";
     }
+}
+?
